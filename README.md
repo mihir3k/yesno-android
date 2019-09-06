c5ff705a942c52ffe33b5c34325b965bdc0c8c7b
Mihir Desai <mihir3k@gmail.com>
2019-09-06 20:42:44 +0530

    Minor changes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Android client for https://yesno.wtf/api
 
-[Download Debug APK](https://github.com/mihir3k/yesno-android/releases)
+[Download APK](https://github.com/mihir3k/yesno-android/releases)
--- app/src/main/java/com/mihir3k/yesno/Model.kt
+++ app/src/main/java/com/mihir3k/yesno/Model.kt
@@ -1,7 +1,7 @@
 package com.mihir3k.yesno
 
 data class Result(
-        val answer: String,
-        val forced: String,
-        val image: String
+    val answer: String,
+    val forced: String,
+    val image: String
 )
